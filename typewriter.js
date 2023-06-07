c824ca4c58156fb10bd6c0e476707de669d28767
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-07 02:49:46 +0000

    “typewriter“

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-plusplus */
+const sentence = 'hello there from lighthouse labs';
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, i * 50);
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, sentence.length * 50);
